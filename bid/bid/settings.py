5143c34cb6e3c14d45046bee95ab6219b10e602a
alimozzamandurjoy <alimozzamandurjoy@gmail.com>
2022-04-13 02:00:41 +0600

    static

--- bid/bid/settings.py
+++ bid/bid/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ldw5^azaf(umlmkzwmkq(kve4+08%+^k54^d9&v3i9+i1*@k@h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
